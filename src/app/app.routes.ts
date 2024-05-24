b809f31a31a36821d8f98adf71ad1f56b75c042b
Johnny Gérard <john@jgerard.dev>
2024-05-24 22:04:53 +0200

    feat: routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    component: HomeComponent,
+  },
+  {
+    path: "register",
+    loadComponent: () =>
+      import("./pages/registration/registration.component").then(
+        (module) => module.RegistrationComponent,
+      ),
+  },
+  {
+    path: "**",
+    redirectTo: "",
+  },
+];
